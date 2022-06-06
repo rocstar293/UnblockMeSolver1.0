4b4c644fd78b3c40d32f86d8ad1e4fa3e074e6b1
Tyler Beaird <tyler.beaird@aecom.com>
2022-06-06 17:09:02 -0400

    Refactored block to use a hashmap so searches take O(1)time

--- Classes.cpp
+++ Classes.cpp
@@ -1,13 +1,9 @@
 #include "Classes.h"
 
-Gameboard::Gameboard(std::vector<std::vector<char>> _data)
+Gameboard::Gameboard(std::vector<std::vector<char>> a_data)
 {
-	data = _data;
-
+	data = a_data;
 	UpdateBlocks();
-
-	for (int block_cnt = 0; block_cnt < blocks.size(); block_cnt++)
-		blocks[block_cnt].first.Calculate(blocks[block_cnt].second);
 }
 
 void Gameboard::UpdateData()
@@ -17,11 +13,11 @@ void Gameboard::UpdateData()
 		std::fill(data[c].begin(), data[c].end(), '-');
 	}
 
-	for (int i = 0; i < blocks.size(); i++)
+	for (auto block = blocks.begin(); block != blocks.end(); block++)
 	{
-		for (int j = 0; j < blocks[i].second.size(); j++)
+		for (int coord_ind = 0; coord_ind < block->second.coords.size(); coord_ind++)
 		{
-			data[blocks[i].second[j].second][blocks[i].second[j].first] = blocks[i].first.id;
+			data[block->second.coords[coord_ind].first][block->second.coords[coord_ind].second] = block->first;
 		}
 	}
 }
@@ -29,185 +25,87 @@ void Gameboard::UpdateData()
 void Gameboard::UpdateBlocks()
 {
 	blocks.clear();
-	
+
 	for (int c = 0; c < data.size(); c++) {
 		for (int r = 0; r < data[0].size(); r++)
 		{
 			if (data[c][r] == '-')
 				continue;
 
-			std::pair<int, int> coord = { r, c };
-			bool found = false;
+			if (blocks.count(data[c][r]) == 0)
+			{
+				Block NewBlock(data[c][r]);
+				blocks[data[c][r]] = NewBlock;
+			}
 
-			for (int i = 0; i < blocks.size(); i++)
-			{
-				if (blocks[i].first.id == data[c][r])
-				{
-					blocks[i].second.push_back(coord);
-					found = true;
-					break;
-				}
-			}
-			if (found == false)
-			{
-				blocks.push_back(std::pair<Block, std::vector<std::pair<int, int>>>(Block(data[c][r]), std::vector<std::pair<int, int>>({ coord })));
-			}
+			blocks[data[c][r]].coords.push_back({ c, r });
 		}
 	}
-
-	for (int block_cnt = 0; block_cnt < blocks.size(); block_cnt++)
-		blocks[block_cnt].first.Calculate(blocks[block_cnt].second);
+	for (auto block = blocks.begin(); block != blocks.end(); block++)
+		block->second.Calculate();
 }
 
 std::vector<std::pair<char, std::vector<int>>> Gameboard::GetAllMoves()
 {
 	std::vector<std::pair<char, std::vector<int>>> _movelist;
 
-	for (int i = 0; i < blocks.size(); i++)
+	for (auto block = blocks.begin(); block != blocks.end(); block++)
 	{
-		_movelist.push_back(blocks[i].first.GetMoves(blocks[i].second, data));
+		_movelist.push_back({ block->first, block->second.GetMoves(data) });
 	}
-	
+
 	return _movelist;
 }
 
-void Gameboard::MoveBlock(std::pair<char, int> _move)
+void Gameboard::MoveBlock(std::pair<char, int> a_move)
 {
-	for (int i = 0; i < blocks.size(); i++)
-	{
-		if (blocks[i].first.id == _move.first)
-		{	
+	Block& block = blocks[a_move.first];
 
-			if (blocks[i].first.is_horizontal)
-			{
-				for (int j = 0; j < blocks[i].second.size(); j++)
-				{
-					blocks[i].second[j] = std::pair<int, int>{ blocks[i].second[j].first + _move.second, blocks[i].second[j].second };
-				}
-			}
-			else
-			{
-				for (int j = 0; j < blocks[i].second.size(); j++)
-				{
-					blocks[i].second[j] = std::pair<int, int>{ blocks[i].second[j].first, blocks[i].second[j].second + _move.second };
-				}
-			}
+	if (block.is_horizontal)
+	{
+		for (int coord = 0; coord < block.coords.size(); coord++)
+		{
+			block.coords[coord] = { block.coords[coord].first, block.coords[coord].second + a_move.second };
 		}
 	}
+
+	else
+	{
+		for (int coord = 0; coord < block.coords.size(); coord++)
+		{
+			block.coords[coord] = { block.coords[coord].first + a_move.second, block.coords[coord].second };
+		}
+	}
+
 	UpdateData();
 }
 
-std::pair<std::string, std::string> Gameboard::SaveState(std::string _parent_state)
+std::pair<std::string, std::string> Gameboard::SaveState(std::string a_parent_state)
 {
 	std::string gamestate = "";
-	
+
 	for (int c = 0; c < data.size(); c++) {
 		for (int r = 0; r < data[0].size(); r++)
 		{
 			gamestate += data[c][r];
 		}
 	}
-	gamestate_history[gamestate] = { movecount, _parent_state };
-	
-	return { gamestate, _parent_state };
+	gamestate_history[gamestate] = { movecount, a_parent_state };
+
+	return { gamestate, a_parent_state };
 }
 
-void Gameboard::GoToState(std::string _state)
+void Gameboard::GoToState(std::string a_state)
 {
-	int save_iter = 0;
+	int char_ind = 0;
 	for (int c = 0; c < data.size(); c++) {
 		for (int r = 0; r < data[0].size(); r++)
 		{
-			data[c][r] = _state[save_iter];
-			save_iter++;
+			data[c][r] = a_state[char_ind];
+			char_ind++;
 		}
 	}
 	UpdateBlocks();
-}
-
-void Gameboard::Solve(bool debug)
-{
-	std::pair<std::string, std::string> prev_state = SaveState("First");
-
-	while (true)
-	{
-		movecount++;
-		std::unordered_map<std::string, std::pair<int, std::string>>::iterator gamestate_it = gamestate_history.begin();
-		
-		if (QuerySolved())
-		{
-			std::cout << "Already Solved!" << std::endl;
-			return;
-		}
-		
-		int new_gamestates = 0;
-		
-		while (gamestate_it != gamestate_history.end())
-		{
-			if (gamestate_it->second.first == movecount - 1)
-			{
-				GoToState(gamestate_it->first);
-				prev_state = { gamestate_it->first , gamestate_it->first };
-				std::vector<std::pair<char, std::vector<int>>> _movelist = GetAllMoves();
-
-				for (int i = 0; i < _movelist.size(); i++)
-				{
-					for (int j = 0; j < _movelist[i].second.size(); j++)
-					{
-						MoveBlock(std::pair<char, int> { _movelist[i].first, _movelist[i].second[j] });
-						if (QueryDuplicateMove() == false)
-						{
-							std::pair<std::string, std::string> current_state = SaveState(prev_state.second);
-
-							if (debug == true)
-							{
-								std::cout << " -------- " << std::endl;
-								PrintBoard();
-							}
-
-							new_gamestates++;
-
-
-							if (QuerySolved())
-							{
-								std::cout << "======== Solution ========" << std::endl;
-								PrintSolution(current_state);
-								return;
-							}
-						}
-						GoToState(prev_state.first);
-					}
-				}
-			}
-			gamestate_it++;
-		}
-		if (new_gamestates == 0)
-		{
-			std::cout << "=== No Solution ===" << std::endl;
-			return;
-		}
-	}
-}
-
-bool Gameboard::QuerySolved()
-{
-	bool is_free = true;
-
-	for (int i = 0; i < blocks.size(); i++)
-	{
-		if (blocks[i].first.id == '#')
-		{
-			for (int j = blocks[i].second[blocks[i].second.size() - 1].first; j < data[blocks[i].second[0].second].size()-1; j++)
-			{
-				if (data[blocks[i].second[0].second][j+1] != '-')
-				{
-					is_free = false;
-					return is_free;
-				}
-			}
-		}
-	}
-	return is_free;
 }
 
 bool Gameboard::QueryDuplicateMove()
@@ -228,6 +126,95 @@ bool Gameboard::QueryDuplicateMove()
 	return is_duplicate;
 }
 
+bool Gameboard::QuerySolved()
+{
+	bool is_free = true;
+
+	Block& block = blocks['#'];
+
+	for (int coord = block.coords[block.coords.size() - 1].second + 1; coord < data[block.coords[0].second].size(); coord++)
+	{
+		if (data[block.coords[0].first][coord] != '-')
+		{
+			is_free = false;
+			return is_free;
+		}
+	}
+	return is_free;
+}
+
+void Gameboard::Solve(bool a_debug)
+{
+	auto start_time = std::chrono::high_resolution_clock::now();
+
+	std::pair<std::string, std::string> prev_state = SaveState("First");
+
+	while (true)
+	{
+		movecount++;
+		std::unordered_map<std::string, std::pair<int, std::string>>::iterator gamestate_it = gamestate_history.begin();
+
+		if (QuerySolved())
+		{
+			std::cout << "Already Solved!" << std::endl;
+			return;
+		}
+
+		int new_gamestates = 0;
+
+		while (gamestate_it != gamestate_history.end())
+		{
+			if (gamestate_it->second.first == movecount - 1)
+			{
+				GoToState(gamestate_it->first);
+
+				prev_state = { gamestate_it->first , gamestate_it->first };
+				std::vector<std::pair<char, std::vector<int>>> movelist = GetAllMoves();
+
+				for (int block = 0; block < movelist.size(); block++)
+				{
+					for (int block_move = 0; block_move < movelist[block].second.size(); block_move++)
+					{
+						MoveBlock(std::pair<char, int> { movelist[block].first, movelist[block].second[block_move] });
+						if (QueryDuplicateMove() == false)
+						{
+							std::pair<std::string, std::string> current_state = SaveState(prev_state.second);
+
+							if (a_debug == true)
+							{
+								std::cout << " -------- " << std::endl;
+								PrintBoard();
+							}
+
+							new_gamestates++;
+
+
+							if (QuerySolved())
+							{
+								auto finish_time = std::chrono::high_resolution_clock::now();
+								std::chrono::duration<double> time_elapsed = finish_time - start_time;
+
+								std::cout << "======== Solution ========" << std::endl;
+								std::cout << "Time Elapsed: " << time_elapsed.count() << std::endl;
+								std::cout << "Moves: " << (gamestate_history[current_state.first].first) + 1 << std::endl;
+								PrintSolution(current_state);
+								return;
+							}
+						}
+						GoToState(prev_state.first);
+					}
+				}
+			}
+			gamestate_it++;
+		}
+		if (new_gamestates == 0)
+		{
+			std::cout << "=== No Solution ===" << std::endl;
+			return;
+		}
+	}
+}
+
 void Gameboard::PrintBoard()
 {
 	for (int c = 0; c < data.size(); c++) {
@@ -239,86 +226,101 @@ void Gameboard::PrintBoard()
 	}
 }
 
-void Gameboard::PrintSolution(std::pair<std::string, std::string> _solved_state)
+void Gameboard::PrintSolution(std::pair<std::string, std::string> a_solved_state)
 {
-	std::vector<std::string> _solution_states = { _solved_state.second };
-	std::pair<int, std::string> _prev_state = gamestate_history[_solved_state.first];
-	while (_prev_state.second != "First")
+	std::vector<std::string> solution_states = { a_solved_state.second };
+	std::pair<int, std::string> prev_state = gamestate_history[a_solved_state.first];
+	while (prev_state.second != "First")
 	{
-		_prev_state = gamestate_history[_prev_state.second];
-		_solution_states.push_back(_prev_state.second);
+		prev_state = gamestate_history[prev_state.second];
+		solution_states.push_back(prev_state.second);
 	}
-	int _step_counter = 0;
-	for (int i = _solution_states.size() - 2; i >= 0; i--)
+	int step_counter = 0;
+	for (int i = solution_states.size() - 2; i >= 0; i--)
 	{
 		std::system("pause");
 		std::system("cls");
-		GoToState(_solution_states[i]);
-		std::cout << "===== " << _step_counter << " =====" << std::endl;
+		GoToState(solution_states[i]);
+		std::cout << "==== " << step_counter << " ====" << std::endl;
 		PrintBoard();
-		_step_counter++;
+		step_counter++;
 	}
-	
+
 	std::system("pause");
 	std::system("cls");
-	GoToState(_solved_state.first);
-	std::cout << "===== " << _step_counter << " =====" << std::endl;
+	GoToState(a_solved_state.first);
+	std::cout << "==== " << step_counter << " ====" << std::endl;
+	PrintBoard();
+	step_counter++;
+
+	std::system("pause");
+	std::system("cls");
+	blocks.erase('#');
+	UpdateData();
+	std::cout << "==== " << step_counter << " ====" << std::endl;
 	PrintBoard();
 }
 
-Block::Block(char _id)
+Block::Block()
 {
-	id = _id;
+	id = 'X';
 	is_horizontal = false;
 }
 
-void Block::Calculate(std::vector<std::pair<int, int>> _coords) 
+Block::Block(char a_id)
 {
-	if (_coords[0].second == _coords[1].second)
+	id = a_id;
+	is_horizontal = false;
+}
+
+
+void Block::Calculate()
+{
+	if (coords[0].first == coords[1].first)
 		is_horizontal = true;
 }
 
-std::pair<char, std::vector<int>> Block::GetMoves(std::vector<std::pair<int, int>> _coords, std::vector<std::vector<char>> _data)
+std::vector<int> Block::GetMoves(std::vector<std::vector<char>> a_data)
 {
-	std::pair<char, std::vector<int>> _moves(id, {});
+	std::vector<int> moves;
 
-	int _back_search;
-	int _front_search;
+	int back_search;
+	int front_search;
 
 	if (is_horizontal)
 	{
-		_back_search = _coords[0].first - 1;
-		_front_search = _coords[_coords.size() - 1].first + 1;
-		
-		while (_back_search > -1 && _data[_coords[0].second][_back_search] == '-')
+		back_search = coords[0].second - 1;
+		front_search = coords[coords.size() - 1].second + 1;
+
+		while (back_search > -1 && a_data[coords[0].first][back_search] == '-')
 		{
-			_moves.second.push_back(_back_search - _coords[0].first);
-			_back_search--;
+			moves.push_back(back_search - coords[0].second);
+			back_search--;
 		}
 
-		while (_front_search < _data[0].size() && _data[_coords[0].second][_front_search] == '-')
+		while (front_search < a_data[0].size() && a_data[coords[0].first][front_search] == '-')
 		{
-			_moves.second.push_back(_front_search - _coords[_coords.size() - 1].first);
-			_front_search++;
+			moves.push_back(front_search - coords[coords.size() - 1].second);
+			front_search++;
 		}
 	}
 	else
 	{
-		_back_search = _coords[0].second - 1;
-		_front_search = _coords[_coords.size() - 1].second + 1;
+		back_search = coords[0].first - 1;
+		front_search = coords[coords.size() - 1].first + 1;
 
-		while (_back_search > -1 && _data[_back_search][_coords[0].first] == '-')
+		while (back_search > -1 && a_data[back_search][coords[0].second] == '-')
 		{
-			_moves.second.push_back(_back_search - _coords[0].second);
-			_back_search--;
+			moves.push_back(back_search - coords[0].first);
+			back_search--;
 		}
 
-		while (_front_search < _data[0].size() && _data[_front_search][_coords[0].first] == '-')
+		while (front_search < a_data[0].size() && a_data[front_search][coords[0].second] == '-')
 		{
-			_moves.second.push_back(_front_search - _coords[_coords.size() - 1].second);
-			_front_search++;
+			moves.push_back(front_search - coords[coords.size() - 1].first);
+			front_search++;
 		}
 	}
 
-	return _moves;
+	return moves;
 }
